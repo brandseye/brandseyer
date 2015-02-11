594f5f875a1b472443b1a1ce1b7f702324c574b4
Rudy Neeser <rudy.neeser@brandseye.com>
2015-02-11 13:26:58 +0200

    Work on reading mentions

--- R/mentions.R
+++ R/mentions.R
@@ -1,7 +1,21 @@
-mentions <- function(account, filter) {
-    url <- paste0("https://api.brandseye.com/rest/accounts/", account$code, "/mentions")
-    data <- httr::GET(url, httr::authenticate(account$auth$user, account$auth$password), 
+#' Read mentions from your account
+mentions <- function(account, ...) {
+    UseMethod("mentions", account)
+}
+
+#' @describeIn mentions
+#' @examples
+#' # Read mentions using the default authentication
+#' mentions("QUIR01BA", "published inthelast day")
+mentions.character <- function(code, filter, authentication = defaultAuthentication) {
+    url <- paste0("https://api.brandseye.com/rest/accounts/", code, "/mentions")
+    data <- httr::GET(url, httr::authenticate(authentication$user, authentication$password), 
                       query = list(filter = filter))    
     results <- jsonlite::fromJSON(httr::content(data, "text"))
-    results
+    results$data
+}
+
+#' @describeIn mentions
+mentions.brandseye.account <- function(account, filter) {
+    mentions(account$code, filter, account$auth)
 }

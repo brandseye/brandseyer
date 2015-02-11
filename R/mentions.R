64e458f557e18b33df62d2d6924b295fb194ba8a
Rudy Neeser <rudy.neeser@brandseye.com>
2015-02-11 14:00:55 +0200

    Added offset and limit

--- R/mentions.R
+++ R/mentions.R
@@ -4,13 +4,18 @@ mentions <- function(account, ...) {
 }
 
 #' @describeIn mentions
+#' @param limit The maximum number of mentions to be returned
+#' @param offset Mentions are returned in an order. Offset says how many of the 
+#'   first mentions should be skipped.
 #' @examples
 #' # Read mentions using the default authentication
 #' mentions("QUIR01BA", "published inthelast day")
-mentions.character <- function(code, filter, authentication = defaultAuthentication) {
+mentions.character <- function(code, filter, 
+                               limit = 10000, offset = NULL,
+                               authentication = defaultAuthentication) {
     url <- paste0("https://api.brandseye.com/rest/accounts/", code, "/mentions")
     data <- httr::GET(url, httr::authenticate(authentication$user, authentication$password), 
-                      query = list(filter = filter))    
+                      query = list(filter = filter, limit = limit, offset = offset))    
     results <- jsonlite::fromJSON(httr::content(data, "text"))
     results$data
 }

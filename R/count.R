fd685e35969e07566a774e1ff9943b09af8929a2
Rudy Neeser <rudy.neeser@brandseye.com>
2015-02-24 13:09:53 +0200

    Now parallelises the count endpoint

--- R/count.R
+++ R/count.R
@@ -18,6 +18,20 @@ count <- function(account, ...) {
 #' @param filter A filter string describing the mentions that should be counted by this query
 #' Count aggregate mention information from your BrandsEye account
 #' @param groupby A list of items that should be grouped by
+#' 
+#' @details 
+#' It's possible to parallelise this call. This is only useful if you're querying multiple accounts:
+#' there will be no benefit when querying only a single account. Any parallel backend for 
+#' the \code{foreach} package can be used to enable parallel functioning. For example, on
+#' a Linux or OX X based system, the following will work well:
+#' 
+#' \verb{
+#'  library(doMC)
+#'  registerDoMC(8)
+#'  count(listAccountCodes(), "published inthelast day")
+#' }
+#' 
+#' 
 #' @examples
 #' count("QUIR01BA", "published inthelast month") # Uses default authentication, 
 #'                                                # if that has been set up.
@@ -56,10 +70,10 @@ count.character <- function(accounts,
         return(results)
     }    
     
-    Reduce(rbind, lapply(accounts, function(code) {
+    foreach(code = accounts, .combine = rbind) %dopar% {
         data <- count(code, filter, groupby, include, authentication)
-        data <- cbind(code = code, data)
-    }))        
+        data.frame(code = code, data)
+    }    
 }
 
 #' @describeIn count

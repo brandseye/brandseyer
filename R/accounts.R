6bbc089581e59d66e07bd382c95597d11ac92bca
Rudy Neeser <rudy.neeser@brandseye.com>
2015-04-10 11:35:09 +0200

    Updated documentation and added a license

--- R/accounts.R
+++ R/accounts.R
@@ -1,3 +1,24 @@
+# Copyright (c) 2015, Brandseye PTY (LTD) 
+# 
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#     
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 #' Provides access to a BrandsEye account
 #' 
 #' @details
@@ -5,10 +26,13 @@
 #' various queries on the account. 
 #' 
 #' @examples
+#' \dontrun{
 #' ac <- account("QUIR01BA", user = "rudy.neeser@@brandseye.com", 
 #'               password = "This is not my real password")
 #' account("QUIR01BA", key="<api key here>")              
-account <- function (code, auth = pkg.env$defaultAuthentication, key = NULL, user = NULL, password = NULL) {        
+#' }
+account <- function (code, key = NULL, user = NULL, password = NULL,
+                     auth = pkg.env$defaultAuthentication) {        
     if (is.null(auth)) auth <- authentication(user = user, password = password, key = key)
     ac <- structure(list(
         code = code,

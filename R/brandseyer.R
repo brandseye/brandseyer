37b96d2ee28f3627727c1e47722db4163ca7fdae
Rudy Neeser <rudy.neeser@gmail.com>
2015-04-11 21:01:32 +0200

    Creating more package documentation

---
+++ R/brandseyer.R
@@ -0,0 +1,11 @@
+#' brandseyer: access and perform analytics on your BrandsEye accounts
+#'
+#' BrandsEye is a company that provides online monitoring, analytics,
+#' and insights for social media data. We provide an extensive JSON / RESTful API
+#' to access your data. This library is a wrapper for that data, providing 
+#' easy access to the data in your various BrandsEye accounts for use in 
+#' any R analyses and data visualisations. 
+#'
+#' @docType package
+#' @name brandseyer
+NULL

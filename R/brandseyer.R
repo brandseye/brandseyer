5a082a3f6c28d629b3d5b3c3b7843baacc26e526
Rudy Neeser <rudy.neeser@gmail.com>
2015-04-28 17:37:53 +0200

    Updated documentation

--- R/brandseyer.R
+++ R/brandseyer.R
@@ -5,6 +5,11 @@
 #' to access your data. This library is a wrapper for that data, providing 
 #' easy access to the data in your various BrandsEye accounts for use in 
 #' any R analyses and data visualisations. 
+#' 
+#' The home page is https://github.com/brandseye/brandseyer/
+#' 
+#' If you have found a bug in the library, you can report it using the 
+#' library's GitHub issue tracker: https://github.com/brandseye/brandseyer/issues
 #'
 #' @docType package
 #' @name brandseyer
